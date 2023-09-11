b8002accb5c686bc908d64b94e282fa59b9df52f
JTM01123 <101955302+JTM01123@users.noreply.github.com>
2023-09-11 13:15:27 -0700

    Update FacebookDash.py

--- FacebookDash.py
+++ FacebookDash.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from numerize.numerize import numerize
 
+st.set_page_config(page_title = 'Facebook Ad Campaign Dashboard', layout="wide", initial_sidebar_state="collapsed")
+
+@st.cache
+def get_data():
+  
