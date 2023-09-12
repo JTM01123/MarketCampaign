6f35a5507b6dc2b1b53445a93b21c3b63f4f5956
JTM01123 <101955302+JTM01123@users.noreply.github.com>
2023-09-11 17:09:23 -0700

    Update FacebookDash.py

--- FacebookDash.py
+++ FacebookDash.py
@@ -61,3 +61,26 @@ with total4:
 
 with total5:
   st.metric(label = 'Approved Conversions :ballot_box_with_check:', value = numerize(total_approved_conversions))
+
+Q1,Q2 = st.columns(2)
+
+with Q1:
+  FB_df2 = FB_df1.groupby( by = ['campaign']).sum()[['Impressions', 'Clicks']].rest_index()
+  FB_df2['CTR'] = round(FB_df2['Clicks']/FB_df2['Impressions'] *100, 3)
+  fig_CTR_by_campaign = px.bar(FB_df2,
+                            x='campaign',
+                            y='CTR',
+                            title='<b>Click Through Rate</b>')
+    fig_CTR_by_campaign.update_layout(title = {'x' : 0.5},
+                                    plot_bgcolor = "rgba(0,0,0,0)",
+                                    xaxis =(dict(showgrid = False)),
+                                    yaxis =(dict(showgrid = False)))
+    st.plotly_chart(fig_CTR_by_campaign,use_container_width=True)
+                        
+                                    
+                                  
+with Q2:
+  
+
+  
+Q3,Q4 = st.columns(2)
